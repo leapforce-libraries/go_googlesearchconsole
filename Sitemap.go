897e860201387935eb4bab1f469deb6937782b9c
caselongo <caselongo@gmail.com>
2021-10-06 09:38:29 +0200

    added Sitemap

---
+++ Sitemap.go
@@ -0,0 +1,59 @@
+package googlesearchconsole
+
+import (
+	"fmt"
+	"net/url"
+
+	errortools "github.com/leapforce-libraries/go_errortools"
+	g_types "github.com/leapforce-libraries/go_googlesearchconsole/types"
+	go_http "github.com/leapforce-libraries/go_http"
+	go_types "github.com/leapforce-libraries/go_types"
+)
+
+type GetSitemapsResponse struct {
+	Sitemap []Sitemap `json:"sitemap"`
+}
+
+type Sitemap struct {
+	Path            string                 `json:"path"`
+	LastSubmitted   g_types.DateTimeString `json:"lastSubmitted"`
+	IsPending       bool                   `json:"isPending"`
+	IsSitemapsIndex bool                   `json:"isSitemapsIndex"`
+	Type            string                 `json:"type"`
+	LastDownloaded  g_types.DateTimeString `json:"lastDownloaded"`
+	Warnings        go_types.Int64String   `json:"warnings"`
+	Errors          go_types.Int64String   `json:"errors"`
+	Contents        []SitemapContent       `json:"contents"`
+}
+
+type SitemapContent struct {
+	Type      string               `json:"type"`
+	Submitted go_types.Int64String `json:"submitted"`
+	Indexed   go_types.Int64String `json:"indexed"`
+}
+
+type GetSitemapsConfig struct {
+	SiteURL      string
+	SitemapIndex *string
+}
+
+func (service *Service) GetSitemaps(config *GetSitemapsConfig) (*[]Sitemap, *errortools.Error) {
+	values := url.Values{}
+
+	if config.SitemapIndex != nil {
+		values.Set("sitemapIndex", *config.SitemapIndex)
+	}
+
+	getSitemapsResponse := GetSitemapsResponse{}
+
+	requestConfig := go_http.RequestConfig{
+		URL:           service.url(fmt.Sprintf("sites/%s/sitemaps?%s", url.QueryEscape(config.SiteURL), values.Encode())),
+		ResponseModel: &getSitemapsResponse,
+	}
+	_, _, e := service.googleService.Get(&requestConfig)
+	if e != nil {
+		return nil, e
+	}
+
+	return &getSitemapsResponse.Sitemap, nil
+}
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/leapforce-libraries/go_errortools v0.0.0-20210628193911-6d614fa4274a
 	github.com/leapforce-libraries/go_google v0.0.0-20210722093139-073b0a9a43b0
 	github.com/leapforce-libraries/go_http v0.0.0-20210713192036-b809589a39a5
+	github.com/leapforce-libraries/go_types v0.0.0-20210415100703-687744db463d
 )
